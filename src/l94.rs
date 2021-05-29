85faad43cf025dcf0f9ef9bb0ce08847771655d1
leemaster <leemast3r@outlook.com>
2021-05-30 06:36:32 +0800

    create the project and slove the leetcode 94

---
+++ src/l94.rs
@@ -0,0 +1,63 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self{
+        TreeNode{
+            val,
+            left: None,
+            right: None
+        }
+    }
+}
+
+pub struct Solution;
+
+impl Solution {
+    pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        fn traverse(root: Option<Rc<RefCell<TreeNode>>>, counter: &mut Vec<i32>){
+            if let Some(node) = root{
+                traverse(node.borrow_mut().left.take(), counter);
+                counter.push(node.borrow().val);
+                traverse(node.borrow_mut().right.take(), counter);
+            }
+        }
+
+        let mut counter = vec![];
+        traverse(root, &mut counter);
+        counter
+    }
+
+    // TODO: implement the traverse not recesive method to calculate the result using stack 
+}
+
+#[cfg(test)]
+mod tests {
+    use std::rc::Rc;
+    use std::cell::RefCell;
+    use crate::l94::Solution;
+    use crate::l94::TreeNode;
+
+    #[test]
+    fn example_test(){
+        let root = Some(Rc::new(RefCell::new(TreeNode::new(1))));
+        // if ref keyword is not exists the compiler will complain the partially moved of root
+        if let Some(ref node) = root {
+            node.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode::new(2))));
+            if let Some(ref node) = node.borrow_mut().right{
+                node.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode::new(3))));
+            }
+        } 
+
+        let result = Solution::inorder_traversal(root);
+        assert_eq!(result, vec![1, 3, 2]);
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+mod l94;
+fn main() {
+    println!("Hello, world!");
+}
